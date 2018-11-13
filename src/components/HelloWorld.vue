49c695c89f2af18824c6c4cdf425f8787c512d5b
Alessandro Schneider <alessandro.schneider.privat@gmail.com>
2018-11-13 12:24:24 +0100

    counter works

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="hello">
     {{ $store.state.count }}
+    Counter is : {{ evenOrOdd }}
+    <button v-on:click="increment()">Add 1</button>
+    <button v-on:click="decrement()">Minus 1</button>
   </div>
 </template>
 
 <script>
+  import {mapGetters, mapActions} from 'vuex'
+
   export default {
     name: 'HelloWorld',
-    data() {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
-    }
+    computed: mapGetters([
+      'evenOrOdd'
+    ]),
+    methods: mapActions([
+      'increment',
+      'decrement'
+    ])
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,31 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  count: 0
+  count: 1
 }
 
+const getters = {
+  evenOrOdd: state => state.count % 2 == 0 ? 'even' : 'odd'
+}
+
+const mutations = {
+  increment(state) {
+    state.count++
+  },
+  decrement(state) {
+    state.count--
+  }
+}
+
+const actions = {
+  increment: (context, payload) => context.commit('increment'),
+  decrement: (context, payload) => context.commit('decrement')
+}
+
+
 export default new Vuex.Store({
-  state
+  state,
+  getters,
+  mutations,
+  actions
 })
